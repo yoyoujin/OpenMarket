9e335f401c09078ea88bb216502c3e05c77deb00
youjin <yjinhann@gmail.com>
2023-02-26 23:59:15 +0900

    feat: header 마크업

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Logo from '../../assets/images/Logo-hodu.png';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>
+          <img src={Logo} alt="호두마켓 로고 이미지" />
+        </h1>
+        <form>
+          <input type="text" placeholder="상품을 검색해보세요!" />
+          <button className="search-btn" type="button"></button>
+        </form>
+        <div className="header-icons">
+          <div>
+            <img src="" alt="" /> 장바구니
+          </div>
+          <div>
+            <img src="" alt="" /> 로그인
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
