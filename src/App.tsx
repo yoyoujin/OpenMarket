7f80bb3f8e2b828c7350973ec6f9a6cbec885581
youjin <yjinhann@gmail.com>
2023-02-28 23:12:06 +0900

    feat: NotFound 페이지 생성 및 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import RootLayout from './pages/RootLayout';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import RootLayout from './pages/RootLayout/RootLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <NotFound />,
     element: <RootLayout />,
     children: [{ path: '/', element: <Home /> }],
   },
